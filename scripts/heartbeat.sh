94043193991b672a5b0e5329ea964772719a1f91
Phạm Đình Trung <phamdinhtrung46@gmail.com>
2026-06-05 13:50:28 +0700

    Add heartbeat script to send metrics to CloudWatch

--- scripts/heartbeat.sh
+++ scripts/heartbeat.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+INSTANCE_ID="on-prem-vm"
+aws cloudwatch put-metric-data \
+  --namespace "HospitalDR" \
+  --metric-name "Heartbeat" \
+  --value 1 \
+  --dimensions "InstanceId=$INSTANCE_ID" \
+  --region ap-southeast-1
